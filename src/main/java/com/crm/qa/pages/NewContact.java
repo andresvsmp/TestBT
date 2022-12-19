aa0c5186a6c356cce4adfc00c2307794b87890a2
DESKTOP-KVBI5FO\user <vasquezandres@javeriana.edu.co>
2022-12-19 10:20:56 -0500

    Test relacionado página https://serenity.is/demo/ update 4

--- src/main/java/com/crm/qa/pages/NewContact.java
+++ src/main/java/com/crm/qa/pages/NewContact.java
@@ -1,16 +1,9 @@
 package com.crm.qa.pages;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.util.concurrent.TimeUnit;
 
 public class NewContact {
 
