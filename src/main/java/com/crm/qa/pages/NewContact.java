fdfd3db726987cadacd12edcaa09c0ec6c975f67
DESKTOP-KVBI5FO\user <vasquezandres@javeriana.edu.co>
2022-12-18 20:50:09 -0500

    Initial commit, Test demo.serenity webSite

---
+++ src/main/java/com/crm/qa/pages/NewContact.java
@@ -0,0 +1,43 @@
+package com.crm.qa.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+public class NewContact {
+
+    WebDriver driver;
+
+    //Initializing the page Objects
+    public NewContact(WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver,this);
+    }
+
+    public void newContact(){
+
+        driver.get("https://demo.serenity.is/Account/Login/?ReturnUrl=%2F");
+        driver.navigate().to("https://demo.serenity.is/Organization/Contact");
+        driver.findElement(By.xpath("//*[@class='tool-button add-button icon-tool-button']")).click();
+
+        driver.findElement(By.name("Title")).sendKeys("Mr");
+        driver.findElement(By.name("FirstName")).sendKeys("Andres");
+        driver.findElement(By.name("LastName")).sendKeys("Vasquez");
+        driver.findElement(By.name("Email")).sendKeys("andresvasquez@gmail.com");
+        driver.findElement(By.name("IdentityNo")).sendKeys("12345678");
+
+        driver.findElement(By.xpath("//*[@class='fa fa-check-circle text-purple']")).click();
+
+    }
+
+
+
+}
